e43fa3e57d2b0cc4e4b7e1d369bad98428ba2352
Roman <gobkins@gmail.com>
2019-09-16 10:44:57 -0400

    pallete tweaks

--- src/Components/ColorFolder/ColorPalette.js
+++ src/Components/ColorFolder/ColorPalette.js
@@ -1,52 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import './ColorPalette.scss';
 
-function ColorPalette (props) {
-    const colorArray = props.colorArray;
-    const convertedArray = [];
+class ColorPalette extends Component{
 
-    // this one here flattens the objects in color data;
-    colorArray.forEach(colorItem => {
-        if (!(/^[a-zA-Z]/.test(colorItem.hex_value))){
-            if (/,/.test(colorItem.hex_value)){
-                const subArray = colorItem.hex_value.split(',');
-                subArray.forEach( color =>{
-                    convertedArray.push(color);
-                });
-            } else {
-                convertedArray.push(colorItem.hex_value);
-            }
+    constructor(){
+        super();
+        this.state = {
+            height: '25px'
         }
-    });
-
-
+    }
     // This one here removes duplicates from the color data that STUPID API provides.
-    const reducedArray = convertedArray.reduce((unique, item) => {
-        return unique.includes(item) ? unique : [...unique, item]
-    }, []);
 
-    console.log(reducedArray);
+    render(){
+        const colorArray = this.props.colorArray;
+            const convertedArray = [];
+    
+            // this one here flattens the objects in color data;
+            colorArray.forEach(colorItem => {
+                if (!(/^[a-zA-Z]/.test(colorItem.hex_value))){
+                    if (/,/.test(colorItem.hex_value)){
+                        const subArray = colorItem.hex_value.split(',');
+                        subArray.forEach( color =>{
+                            convertedArray.push(color);
+                        });
+                    } else {
+                        convertedArray.push(colorItem.hex_value);
+                    }
+                }
+            });
 
-    return(
-        <ul>
-            {reducedArray.map(color => {
-                const ColorBox = styled.div`
-                    background:${color};
-                    width:25px;
-                    height:25px;
-                    border:1px solid black;
-                    border-radius:50%;
-                `
-                return(
-                    <li>
-                        <ColorBox className="colorBox"> </ColorBox>
-                    </li>
-                );
-            })}
-            
-        </ul>
-    )
+        const reducedArray = convertedArray.reduce((unique, item) => {
+            return unique.includes(item) ? unique : [...unique, item]
+        }, []);
+
+        return(
+            <ul>
+                {reducedArray.map(color => {
+                    const ColorBox = styled.div`
+                        background:${color};
+                        width:25px;
+                        height:25px;
+                        border:1px solid black;
+                        border-radius:50%;
+                    `
+                    return(
+                        <li>
+                            <ColorBox className="colorBox"> </ColorBox>
+                        </li>
+                    );
+                })}
+                
+            </ul>
+        )
+    }
+
 }
 
 export default ColorPalette;
